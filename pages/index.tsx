b55583586466a310c8f89aacf1ebd603c31d2eb6
DaisyB_ell <alina_borz@mail.ru>
2022-11-05 21:58:13 +0300

    Полностью рабочий прототип

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { SearchOutlined } from '@ant-design/icons';
-import {Button, Input, Typography} from 'antd';
+import {Button, Input, Typography, Spin} from 'antd';
 import axios from 'axios';
 import type { NextPage } from 'next';
 import {useState} from "react";
@@ -8,14 +8,24 @@ const Home: NextPage = () => {
 
     const [url, setUrl] = useState('')
     const [isError, setIsError] = useState(false)
+    const [isLoading, setLoading] = useState(false)
 
     const searchManga = async () => {
         try{
             setIsError(false)
-            const response = await axios.get('http://127.0.0.1:5000', {
+            // axios.get(`http://127.0.0.1:5000/image.jpeg`)
+            setLoading(true)
+            const {data} = await axios.post('http://127.0.0.1:5000', {
                 url
             })
-            console.log('response', response)
+            setLoading(false)
+            let element = document.createElement('a');
+            element.setAttribute('href', `http://127.0.0.1:5000/${data}`);
+            element.setAttribute('download', data);
+            console.log(element)
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
         }catch (e){
             setIsError(true)
             setUrl('')
@@ -33,14 +43,19 @@ const Home: NextPage = () => {
                 Вставте ссылку на главу вашей манги, которую надо своровать
             </h2>
 
-            <Input value={url} onChange={(e)=>setUrl(e.target.value)} className="mb-3 mt-3" size="large" placeholder="Ссылка на главу" prefix={<SearchOutlined />} />
+            <Input disabled={isLoading} value={url} onChange={(e)=>setUrl(e.target.value)} className="mb-3 mt-3" size="large" placeholder="Ссылка на главу" prefix={<SearchOutlined />} />
 
-            <div className="flex justify-center">
-                <Button onClick={searchManga} disabled={!url} size="large" type="primary" className='max-w-xs'>
-                    Начать обработку
-                </Button>
-            </div>
-            {isError ? <Typography className="mt-5">Произошла ошибка, попробуйте еще раз или обратитесь к создателю</Typography> : <></>}
+            {
+                isLoading
+                ? <Spin size="large" />
+                : <div className="flex justify-center">
+                    <Button onClick={searchManga} disabled={!url || isLoading} size="large" type="primary" className='max-w-xs'>
+                        Начать обработку
+                    </Button>
+                </div>
+            }
+            {isError ? <Typography className="mt-5 color-red">Произошла ошибка, попробуйте еще раз или обратитесь к создателю</Typography> : <></>}
+            {/* {resUrl ? <a href={`http://127.0.0.1:5000/${resUrl}`}>save</a> : <></>} */}
         </div>
     );
 };
